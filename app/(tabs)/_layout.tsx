f0e8b3c5502d4c729a8dce4ad6f46d3e36d86360
Lordran <lordran@lordran.dev>
2025-09-05 17:52:44 -0600

    Pages placehoders

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,10 @@ export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="index" />
+      <Tabs.Screen name="bookmarks" />
+      <Tabs.Screen name="create" />
+      <Tabs.Screen name="notifications" />
+      <Tabs.Screen name="profile" />
     </Tabs>
   );
 }
---
+++ app/(tabs)/bookmarks.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function Bookmarks() {
+    return (
+        <View>
+            <Text>Bookmarks</Text>
+        </View>
+    )
+}
