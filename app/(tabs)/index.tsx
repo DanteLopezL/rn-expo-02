93d9897f6b7f06fb176cde4fba57aba4703a42fe
Lordran <lordran@lordran.dev>
2025-09-05 17:31:25 -0600

    Pages placehoders

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text>Quiero sexoooo con todas las prras del congal</Text>
       <Text>Traigo cash, traigo hash, cush y estilo original</Text>
     </View>
   );
 }
-
-
