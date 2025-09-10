e3f12ea6dcc03fcd728188380a8f4f71bf4d0130
Lordran <lordran@lordran.dev>
2025-09-09 23:46:21 -0600

    New feed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import { styles } from "@/assets/styles/feed.styles";
 import { COLORS } from "@/assets/styles/theme";
+import Story from "@/components/Story";
+import { STORIES } from "@/constants/mock-data";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
@@ -10,6 +12,12 @@ export default function Index() {
   const handleSignOut = () => {
     router.replace("/(auth)/login");
   };
+
+  const posts = [];
+
+  if (posts.length === 0) {
+    return;
+  }
 
   return (
     <View style={styles.container}>
@@ -19,6 +27,33 @@ export default function Index() {
           <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
         </TouchableOpacity>
       </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.storiesContainer}
+        >
+          {STORIES.map((story) => (
+            <Story key={story.id} story={story} />
+          ))}
+        </ScrollView>
+
+        {}
+      </ScrollView>
     </View>
   );
 }
+
+const NoPostsFound = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: COLORS.background,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text style={{ fontSize: 20, color: COLORS.primary }}>No posts yet</Text>
+  </View>
+);
